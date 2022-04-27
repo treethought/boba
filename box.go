dce5c14c43a570c892505df599b7f0eaa03bd797
Cam Sweeney <cpsweene@gmail.com>
2022-04-27 09:08:20 -0700

    initial work for box model

---
+++ box.go
@@ -0,0 +1,160 @@
+package boba
+
+import (
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var boyStyle = lipgloss.NewStyle().Padding(0).
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.AdaptiveColor{Light: "#B793FF", Dark: "#AD58B4"})
+
+type BoxNode struct {
+	tea.Model
+	// Height represents the percent of node's parent height to fill
+	SizeX int
+	// Width represents the percent of node's parent width to fill
+	SizeY int
+	lines []string
+}
+
+func (n *BoxNode) Init() tea.Cmd {
+	return nil
+}
+
+func (n *BoxNode) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	m, cmd := n.Model.Update(msg)
+
+	node := &BoxNode{
+		Model: m,
+		SizeY: n.SizeY,
+		SizeX: n.SizeX,
+	}
+	return node, cmd
+}
+
+type joinFunc func(n BoxNode, join func(pos lipgloss.Position, strs ...string) string)
+
+type Box struct {
+	nodes []*BoxNode
+	// Percentage of Window height
+	SizeX int
+	// Percentage of Window width
+	SizeY int
+
+	width  int
+	height int
+	ready  bool
+	// joinFunc func(pos lipgloss.Position, strs ...string) string
+	orientation string
+	style       lipgloss.Style
+}
+
+func NewBox(orientation string, x, y int) *Box {
+	return &Box{
+		nodes:       []*BoxNode{},
+		SizeX:       x,
+		SizeY:       y,
+		orientation: orientation,
+		style:       boyStyle,
+	}
+}
+
+// AddNode adds a new node with h and w percent of parent height and width
+func (m *Box) AddNode(n tea.Model, h, w int) {
+	node := &BoxNode{
+		Model: n,
+		SizeY: h,
+		SizeX: w,
+	}
+	m.nodes = append(m.nodes, node)
+}
+
+func (m *Box) Init() tea.Cmd {
+	cmds := []tea.Cmd{}
+	for _, n := range m.nodes {
+		cmds = append(cmds, n.Init())
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m *Box) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmds []tea.Cmd
+	)
+	nodes := []*BoxNode{}
+	for _, n := range m.nodes {
+		nmod, cmd := n.Update(msg)
+		if cmd != nil {
+			cmds = append(cmds, cmd)
+		}
+
+		nNode, ok := nmod.(*BoxNode)
+		if !ok {
+			continue
+		}
+		nodes = append(nodes, nNode)
+	}
+	m.nodes = nodes
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		return m, nil
+
+	case tea.WindowSizeMsg:
+		x, y := m.style.GetFrameSize()
+
+		m.width = int(float64(msg.Width) * (float64(m.SizeX/100) - float64(x)))
+		m.height = int(float64(msg.Height) * (float64(m.SizeY/100) - float64(y)))
+
+		if !m.ready {
+			m.ready = true
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m *Box) View() string {
+
+	out := ""
+
+	for _, n := range m.nodes {
+
+		targetWidth := int(float64(m.width) * float64(n.SizeX/100))
+		targetLines := int(float64(m.height) * float64(n.SizeY/100))
+
+		nodeContent := n.View()
+
+		s := strings.ReplaceAll(nodeContent, "\r\n", "\n") // normalize line endings
+
+		nodeStyle := lipgloss.NewStyle().
+			// BorderStyle(lipgloss.NormalBorder()).
+			// BorderForeground(lipgloss.AdaptiveColor{Light: "#F793FF", Dark: "#AD58B4"}).
+			MaxWidth(targetWidth).MaxHeight(targetLines).Width(targetWidth)
+
+		s = nodeStyle.Render(s)
+
+		if m.orientation == "horizontal" {
+			out = lipgloss.JoinHorizontal(lipgloss.Left, out, s)
+
+		}
+		if m.orientation == "vertical" {
+			out = lipgloss.JoinVertical(lipgloss.Left, out, s)
+		}
+	}
+	return m.style.
+		Width(m.width).Height(m.height).
+		MaxWidth(m.width).MaxHeight(m.height).
+		Render(out)
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/treethought/boba
 go 1.15
 
 require (
-	github.com/charmbracelet/bubbles v0.7.7
-	github.com/charmbracelet/bubbletea v0.13.2
+	github.com/charmbracelet/bubbles v0.10.3
+	github.com/charmbracelet/bubbletea v0.20.0
+	github.com/charmbracelet/lipgloss v0.5.0
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/reflow v0.3.0
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
 )
